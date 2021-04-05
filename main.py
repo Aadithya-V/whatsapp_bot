928b132decd7a1c9396e19fb912a585918cb499f
Aadithya V <Aadithya-V@users.noreply.github.com>
2021-04-05 23:46:33 +0530

    minor clean ups

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from time import sleep
 import pyperclip
 import random 
 
-sleep(2)
+sleep(5) #add a voice "the bot is now functional"
+
 
 position1 = pt.locateOnScreen("images/smiley_paperclip.JPG", confidence = .6)         # confidence is the acceptable look alikeness of the picture
 x = position1[0]
@@ -12,38 +13,27 @@ y = position1[1]
 # Gets message
 
 def get_message():
-    global x, y
-
-    position = pt.locateOnScreen("images/smiley_paperclip.JPG", confidence = .6)
-    x = position1[0]
-    y = position1[1]
-
-    pt.moveTo(x, y, duration = .5)
-    pt.moveTo(x + 70, y - 40, duration = .5)
+    pt.moveTo(x, y, duration = .01)
+    pt.moveTo(x + 90, y - 40, duration = .01)
+    sleep(0.01)
     pt.tripleClick()
+    sleep(0.01)
     pt.rightClick()
-    pt.moveRel(20,-130)
+    pt.moveRel(20,-130, duration=0.01)
+    sleep(0.01)
     pt.click()
     whatssapp_message = pyperclip.paste()
-    print("Message recieved : "+ whatssapp_message)
-
     return whatssapp_message
 
 
 #post response
 
 def post_response(message) :
-    global x,y
-    position = pt.locateOnScreen("images/smiley_paperclip.JPG", confidence = .6)
-    x = position1[0]
-    y = position1[1]
-    pt.moveTo(x + 200, y + 25, duration = .5)
+    pt.moveTo(x + 200, y + 25, duration = .01)
     pt.click()
-    pt.typewrite(message, interval=.01)
+    pt.typewrite(message, interval=.001)
 
-    position_send = pt.locateOnScreen("images/send.JPG", confidence = .9)
-    pt.moveTo(position_send[0] + 20 ,position_send[1] + 20, 0.5)
-    pt.click()
+    pt.typewrite("\n")                  #sending the message \n is interpreted as enter to newline character
 
 
 
